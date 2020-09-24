5bfc8c0cfcbfdbeaf0d6d57eb1365db2e667e485
Jakub <jakubjachowski@icloud.com>
2020-09-24 19:02:20 +0200

    Updates

---
+++ precache-manifest.3a7bda5b0d42e326e33345f714e3a96a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cd045c37803c01656440355ff40758",
+    "url": "/rolodex-udemy/index.html"
+  },
+  {
+    "revision": "6f01c7c412ee0d6c9d73",
+    "url": "/rolodex-udemy/static/css/main.9e439bf8.chunk.css"
+  },
+  {
+    "revision": "78b9060f1495d3485a95",
+    "url": "/rolodex-udemy/static/js/2.33b4488b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex-udemy/static/js/2.33b4488b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f01c7c412ee0d6c9d73",
+    "url": "/rolodex-udemy/static/js/main.962ed1d9.chunk.js"
+  },
+  {
+    "revision": "fda13c28b39a3990c3b4",
+    "url": "/rolodex-udemy/static/js/runtime-main.d5fa960b.js"
+  }
+]);
